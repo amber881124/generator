35fb6327f5ab952fbfc8c2270512fa1327a58172
momo <zyongzhu24@gmail.com>
2023-05-26 18:08:48 +0800

    list comprehension vs. generator expression

--- generator.py
+++ generator.py
@@ -1,20 +1,20 @@
 from memory_profiler import memory_usage # 計算記憶體用量
 import time # 計算程式耗時
 
-# 普通讀檔案
-def readfile():
-    data = []
-    with open('reviews.txt', 'r') as f:
-        for line in f:
-            data.append(line.strip())
-    return data
+# # 普通讀檔案
+# def readfile():
+#     data = []
+#     with open('reviews.txt', 'r') as f:
+#         for line in f:
+#             data.append(line.strip())
+#     return data
 
-# 用generator
-def readfile_gen():
-    with open('reviews.txt', 'r') as f:
-        for line in f:
-            yield line
-    return line
+# # 用generator
+# def readfile_gen():
+#     with open('reviews.txt', 'r') as f:
+#         for line in f:
+#             yield line
+#     return line
 
 
 # data = readfile()
@@ -25,9 +25,12 @@ def readfile_gen():
 # print(next(data_gen))
 # print(next(data_gen))
 
+
+
 print(f'初始記憶體用量為: {memory_usage()} MB')
 start = time.time()
-data = readfile()
+# list comprehension (清單快寫法)
+data = [line.strip() for line in open('reviews.txt')]
 count = 0
 leng = 0
 for i in data:
@@ -47,7 +50,8 @@ print(f'記憶體用量為: {memory_usage()} MB')
 
 print(f'初始記憶體用量為: {memory_usage()} MB')
 start = time.time()
-data = readfile_gen()
+# generator expression (generator快寫法)
+data = (line.strip() for line in open('reviews.txt'))
 count = 0
 leng = 0
 for i in data:
@@ -62,4 +66,5 @@ print(f'記憶體用量為: {memory_usage()} MB')
 # 共有 1000000 筆留言,平均長度為 366.84585 個字
 # 執行總耗時 1.1762585639953613 秒
 # 記憶體用量為: [22.44921875] MB
+
 
